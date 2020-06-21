940557809c8e3d2bc3c7d4096043d72518100c69
KarmaZA <EVRJON003@myuct.ac.za>
2020-06-21 18:08:30 +0200

    Camera can move
    and small fixes to the game

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class CameraMove : MonoBehaviour
+{
+    public Camera cam;
+    public float moveSpeed = 5f;
+    float move;
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxisRaw("Horizontal");
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if ((cam.transform.position.x > -18 && move < 0) || (move > 0 && cam.transform.position.x < 21))
+        {
+            cam.transform.Translate(new Vector3(moveSpeed * move * Time.fixedDeltaTime, 0, 0));
+        }
+    }
+}
