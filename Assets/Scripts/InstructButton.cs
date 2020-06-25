d01bad94eb07d253ff86e3b26517a80716bc20c5
KarmaZA <EVRJON003@myuct.ac.za>
2020-06-25 08:04:18 +0200

    Scene Management done
    And so is build order now to fix some of the play bugs

---
+++ Assets/Scripts/InstructButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InstructButton : MonoBehaviour
+{
+    public Button button;
+    public Text intro;
+    public Text control;
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            button.onClick.AddListener(NextPage);
+        }
+        catch
+        {
+
+        }
+    }
+    void NextPage()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour
+{
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            button.onClick.AddListener(LoadNextLevel);
+        }
+        catch
+        {
+            Debug.Log("Button Null Pointer exception");
+        }
+    }
+    void LoadNextLevel()
+    {
+        Application.Quit();
+    }
+}
